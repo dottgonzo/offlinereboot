7c2a77fab8a99d6e74d25b2a5479588308b41a52
Dario Caruso <darioyzf@gmail.com>
2015-11-30 20:28:57 +0100

    oki

--- index.js
+++ index.js
@@ -4,20 +4,10 @@ verb=require('verbo'),
 merge=require('json-add'),
 hwrestart=require('hwrestart');
 
-function offreboot(options){
-  setTimeout(function(){
-    testConnection().then(function(){
-      if(options.repeat){
-offreboot(options);
-      }
-    }).catch(function(){
-    hwrestart(options.mode)
-  })
-},options.repeat)
 
-}
 
 module.exports=function(conf){
+
   var options={
     mode:'unplug',
     repeat:false // false or timer to check
@@ -25,23 +15,19 @@ module.exports=function(conf){
   if(conf){
     merge(options,conf)
   }
-
-
+  
   testConnection().then(function(){
     if(options.repeat){
-
-timerdaemon.post(options.repeat,function(){
-  testConnection().catch(function(){
-  hwrestart(options.mode)
-})
-
-})
-
+      timerdaemon.post(options.repeat,function(){
+        testConnection().catch(function(){
+          hwrestart(options.mode)
+        })
+      })
     }
   }).catch(function(){
-  hwrestart(options.mode)
-})
+    hwrestart(options.mode)
+  })
 
 
 
-  }
+}
